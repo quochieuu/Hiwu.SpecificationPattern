5d93163841beff28c6a965b3e1fd3d4e33b61662
HieuHo <quochieuu@gmail.com>
2024-08-04 19:34:56 +0700

    Add Shared layer

---
+++ Hiwu.SpecificationPattern.Shared/Extensions/StringExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace Hiwu.SpecificationPattern.Shared.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool IsNullOrEmpty(this string value) => string.IsNullOrEmpty(value);
+
+        public static bool IsNullOrWhiteSpace(this string value) => string.IsNullOrWhiteSpace(value);
+
+        public static string ToCamelCase(this string value)
+        {
+            if (string.IsNullOrEmpty(value) || value.Length < 2)
+                return value;
+
+            return char.ToLowerInvariant(value[0]) + value.Substring(1);
+        }
+
+        public static string ToSnakeCase(this string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                return value;
+
+            var sb = new StringBuilder();
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (char.IsUpper(value[i]))
+                {
+                    if (i > 0)
+                        sb.Append('_');
+                    sb.Append(char.ToLowerInvariant(value[i]));
+                }
+                else
+                {
+                    sb.Append(value[i]);
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ Hiwu.SpecificationPattern.Shared/Helpers/EnumHelpers.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+namespace Hiwu.SpecificationPattern.Shared.Helpers
+{
+    public static class EnumHelpers
+    {
+        public static string GetDescription(this Enum enm)
+        {
+            var type = enm.GetType();
+            var memberInfo = type.GetMember(enm.ToString()).FirstOrDefault();
+
+            var descriptionAttribute = memberInfo?
+                .GetCustomAttributes(typeof(DescriptionAttribute), false)
+                .Cast<DescriptionAttribute>()
+                .FirstOrDefault();
+
+            return descriptionAttribute?.Description ?? enm.ToString();
+        }
+
+    }
+}
